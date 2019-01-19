89281d861047ffac1a5e861ce8b9c8a7d27b9c14
thuang123 <thuang_@hotmail.com>
2019-01-18 22:18:50 -0800

    Revert "Add camera scan function using ScanLibrary"
    This reverts commit 4c6e36876521e9104bd3d7f727c84287caa770e4.

--- app/src/main/java/com/project/noteapp/Camera.java
+++ app/src/main/java/com/project/noteapp/Camera.java
@@ -4,13 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
-import android.util.Log;
-
-import com.scanlibrary.ScanActivity;
-import com.scanlibrary.ScanConstants;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,55 +17,42 @@ public class Camera {
     private File storageDir;
     private Activity activity;
     static final int REQUEST_TAKE_PHOTO = 1;
+    private String mCurrentPhotoPath;
     private PackageManager packageManager;
 
-    private static final int REQUEST_OPEN = 99;
-
-    public Camera(Activity activity, PackageManager packageManager) {
+    public Camera(Activity activity, PackageManager packageManager, File storageDir) {
         this.activity = activity;
         this.packageManager = packageManager;
-        this.storageDir = getApplicationStorageDirectory();
-    }
-
-    public File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-
-        if (this.storageDir == null) {
-            this.storageDir = getApplicationStorageDirectory();
-        }
-        File image = new File(this.storageDir.getPath() + File.separator +
-                "IMG_"+ timeStamp + ".jpg");
-        return image;
-    }
-
-    /** Create a File for saving an image on device **/
-    private static File getApplicationStorageDirectory(){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "NoteApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
-                Log.d("NoteApp", "failed to create directory");
-                return null;
-            }
-        }
-        return mediaStorageDir;
+        this.storageDir = storageDir;
     }
 
     public void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(this.activity, ScanActivity.class);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-            int preference = ScanConstants.OPEN_CAMERA;
-            takePictureIntent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
-            this.activity.startActivityForResult(takePictureIntent, REQUEST_OPEN);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(activity,"com.project.noteapp.fileprovider",photoFile));
+            System.out.println("Photo saved into: >>>>>>>>>>>>>>>>>>>>>> " + Uri.fromFile(photoFile).toString());
+            this.activity.startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
         }
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File image = new File(this.storageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 }
--- app/src/main/java/com/project/noteapp/MainActivity.java
+++ app/src/main/java/com/project/noteapp/MainActivity.java
@@ -1,34 +1,31 @@
 package com.project.noteapp;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.project.noteapp.utils.ImageFile;
 import com.project.noteapp.utils.ListAdapter;
-import com.scanlibrary.ScanConstants;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private final int THUMBNAIL_SIZE = 64;
     private final static String TAG = "MainActivity";
-    private final static int REQUEST_CODE = 99;
 
-    private Camera appCamera;
+    //private ArrayList<ImageFile> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +33,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "started successfully. <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                1);
-
         final Button cameraButton = findViewById(R.id.CameraButton);
-        this.appCamera = new Camera(this, getPackageManager());
+        final Camera newCamera = new Camera(this, getPackageManager(), getOutputMediaFile());
         cameraButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                appCamera.dispatchTakePictureIntent();
+                newCamera.dispatchTakePictureIntent();
             }
         });
 
         // Find NoteApp image files
         String noteAppPicturePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()+"/NoteApp";
+
         File directory = new File(noteAppPicturePath);
         File[] files = directory.listFiles();
         if (files != null && files.length != 0) {
@@ -62,25 +56,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
-            try {
-                File photoFile = appCamera.createImageFile();
-                Bitmap bitMap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                // Save scanned bitmap data into created image file
-                FileOutputStream outStream = new FileOutputStream(photoFile);
-                bitMap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-                outStream.flush();
-                outStream.close();
-                // Cleanup temporary data
-                getContentResolver().delete(uri, null, null);
-                System.out.println("Photo was successfully saved into: >>>>>>>>>>>>>>>>>>>>>> " + Uri.fromFile(photoFile).toString());
-            } catch(IOException e) {
-                e.printStackTrace();
+    /** Create a File for saving an image on device **/
+    private static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "NoteApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("NoteApp", "failed to create directory");
+                return null;
             }
         }
+
+        return mediaStorageDir;
     }
 }
