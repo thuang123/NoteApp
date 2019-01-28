aa5d61816ed4eea7bfd579dae5e281c321f88fd0
thuang123 <thuang_@hotmail.com>
2019-01-27 23:50:34 -0800

    Adjust divider spacings

--- app/src/main/java/com/project/noteapp/utils/DividerItemDecoration.java
+++ app/src/main/java/com/project/noteapp/utils/DividerItemDecoration.java
@@ -2,6 +2,7 @@ package com.project.noteapp.utils;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -32,6 +33,14 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
             mDivider.setBounds(left, top, right, bottom);
             mDivider.draw(c);
+        }
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        if (parent.getChildAdapterPosition(view) != parent.getAdapter().getItemCount() - 1) {
+            outRect.bottom = 10;
         }
     }
 }
